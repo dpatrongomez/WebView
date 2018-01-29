519fcfb27d664992d58f1125d468de7efc45eaec
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2018-01-29 12:06:49 +0100

    Create the web

--- app/src/main/java/es/dpatrongomez/webview/MainActivity.java
+++ app/src/main/java/es/dpatrongomez/webview/MainActivity.java
@@ -2,6 +2,7 @@ package es.dpatrongomez.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView web = findViewById(R.id.WebView);
+        web.loadUrl("https://www.dpatrongomez.es/");
     }
 }
