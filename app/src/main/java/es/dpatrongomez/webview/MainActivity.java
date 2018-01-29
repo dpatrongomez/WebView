71f9e0d865ec2fe8ce1a0e39d7f9aafb8811ea45
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2018-01-29 12:37:03 +0100

    Fix Links

--- app/src/main/java/es/dpatrongomez/webview/MainActivity.java
+++ app/src/main/java/es/dpatrongomez/webview/MainActivity.java
@@ -2,7 +2,9 @@ package es.dpatrongomez.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView web = findViewById(R.id.WebView);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         web.loadUrl("https://www.dpatrongomez.es/");
     }
 }
